d7f769e0a3393ff5b722dc773d2cc2de39c3b1d0
Urko Pineda Olmo <urkopineda@gmail.com>
2016-05-27 10:47:32 +0200

    Added config file functionality

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -1,8 +1,6 @@
 package ui;
 
 import api.PSPortFactory;
-import api.PSPortSSL;
-import api.PSPortTCP;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,13 +13,9 @@ import javafx.stage.Stage;
 import middleware.Middleware;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import util.ConfigReader;
 
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 
 /**
  * Class that will manage the whole UI.
@@ -34,15 +28,8 @@ public class UI extends Application {
     private static final String IMAGE_AMBER = "/images/amber.png";
     private static final String IMAGE_GREEN = "/images/green.png";
     private static final String FXML_PATH = "/fxml/design.fxml";
-    private static final String SSL_TYPE = "SSL";
-    private static final String TCP_TYPE = "TCP";
-    private static final int SSL_CODE = 1;
-    private static final int TCP_CODE = 0;
-    private static final int ERROR_CODE = -1;
-    private static final int ADDRESS_INDEX = 1;
-    private static final int PORT_INDEX = 2;
-    private static final int TOPIC_INDEX = 0;
-    private static final int CONN_INDEX = 3;
+    private static final String CONFIG_PATH = "config.dat";
+    private static final String TOPIC_PATH = "topic.dat";
     private Middleware middleware;
 
     @FXML
@@ -53,69 +40,12 @@ public class UI extends Application {
      */
     @Override
     public final void init() throws IOException {
-        middleware = new Middleware(getTopic());
-        if (getConnection() == SSL_CODE) {
-            try {
-                // TODO: Key store, trusted store and key store password.
-                middleware.connect(PSPortFactory.getPort("PSPortSSL -a " + getAddress() + " -p " + getPort()));
-            } catch (Throwable e) {
-                // TODO: Catch exception.
-            }
-        } else if (getConnection() == TCP_CODE) {
-            middleware.connect(new PSPortTCP(getAddress(), Integer.parseInt(getPort())));
-        } else {
-            // TODO: Kill.
+        middleware = new Middleware(new ConfigReader().readConfigFile(TOPIC_PATH));
+        try {
+            middleware.connect(PSPortFactory.getPort("file " + CONFIG_PATH));
+        } catch (Throwable throwable) {
+            LOGGER.fatal("Can't create PSPort.", throwable);
         }
-    }
-
-    /**
-     * This method will take the topic from the arguments of JavaFX.
-     *
-     * @return topic
-     */
-    private String getTopic() {
-        return getParameterAt(TOPIC_INDEX);
-    }
-
-    /**
-     * This method will take the address from the arguments of JavaFX
-     *
-     * @return address
-     */
-    private String getAddress() {
-        return getParameterAt(ADDRESS_INDEX);
-    }
-
-    /**
-     * This method will take the port from the arguments of JavaFX
-     *
-     * @return address
-     */
-    private String getPort() {
-        return getParameterAt(PORT_INDEX);
-    }
-
-    /**
-     * This method will take the connection of the arguments.
-     *
-     * @return connectionType
-     */
-    private int getConnection() {
-        String type = getParameterAt(CONN_INDEX);
-        if ((SSL_TYPE).equals(type)) {
-            LOGGER.info("Stabilising a SSL socket.");
-            return SSL_CODE;
-        } else if ((TCP_TYPE).equals(type)) {
-            LOGGER.info("Stabilising a TCP socket.");
-            return TCP_CODE;
-        } else {
-            LOGGER.fatal("Can't start a " + type + " connection.");
-            return ERROR_CODE;
-        }
-    }
-
-    private String getParameterAt(int index) {
-        return this.getParameters().getRaw().get(index);
     }
 
     /**
