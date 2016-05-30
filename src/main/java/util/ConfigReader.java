179005ca623cc20f95f86b6320e7df0fc776f2ea
Urko Pineda Olmo <urkopineda@gmail.com>
2016-05-30 09:50:31 +0200

    Updated gradle and reader

--- src/main/java/util/ConfigReader.java
+++ src/main/java/util/ConfigReader.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -15,6 +16,8 @@ public class ConfigReader {
 
     private static final Logger LOGGER = LogManager.getLogger(ConfigReader.class);
 
+    private ConfigReader() {}
+
     /**
      * Method reads a config file from a concrete path.
      * This config method must be of a single line.
@@ -24,16 +27,25 @@ public class ConfigReader {
     public static String readConfigFile(String file) {
         Scanner s = null;
         String config = null;
+        FileInputStream inputStream = null;
         try {
-            s = new Scanner(new FileInputStream(file));
+            inputStream = new FileInputStream(file);
+            s = new Scanner(inputStream);
         } catch (FileNotFoundException e) {
             LOGGER.fatal("Can't find the file " + file, e);
         }
+        if (s != null) {
+            try {
+                config = s.nextLine();
+                s.close();
+            } catch (NullPointerException e) {
+                LOGGER.fatal("File " + file + "is null", e);
+            }
+        }
         try {
-            config = s.nextLine();
-            s.close();
-        } catch (NullPointerException e) {
-            LOGGER.fatal("File " + file + "is null", e);
+            inputStream.close();
+        } catch (IOException e) {
+            LOGGER.fatal("Can't close input.", e);
         }
         return config;
     }
